72dae9a8b4fadfde7747a0d8ffbf8e5890a847f3
Vamsi Nellore <vamsi.nellore@brandywineglobal.com>
2019-06-25 17:13:27 -0400

    Enable CORS on api gateway

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,89 +1,194 @@
-resource "aws_api_gateway_rest_api" "MyWebAPIGateway" {
-  name        = "MyWebAPIGateway"
-  description = "LambdaWebAPI API Gateway"
+resource "aws_api_gateway_rest_api" "MyAPIGateway" {
+  name        = "MyAPIGateway"
+  description = "My API Gateway"
 
   endpoint_configuration = {
     types = ["EDGE"]
   }
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  parent_id   = "${aws_api_gateway_rest_api.MyWebAPIGateway.root_resource_id}"
+/// configure {proxy+}
+resource "aws_api_gateway_resource" "proxy_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+////
+resource "aws_api_gateway_method" "proxy_any_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
   http_method   = "ANY"
   authorization = "NONE"
+}
 
-  request_parameters = {
-    "method.request.path.proxy" = true
+resource "aws_api_gateway_method" "proxy_options_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "any_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_any_method.http_method}"
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_options_method.http_method}"
+  status_code = "200"
+
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
 
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_options_method.http_method}"
 
-  integration_http_method = "POST"
+  type = "MOCK"
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200   }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_options_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.options_200.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_integration" "any_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_any_method.http_method}"
+
+  integration_http_method = "POST"                                           //Lambda function can only be invoked with POST
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.LambdaWebAPI.invoke_arn}"
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  resource_id   = "${aws_api_gateway_rest_api.MyWebAPIGateway.root_resource_id}"
+resource "aws_api_gateway_integration_response" "any_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method = "${aws_api_gateway_method.proxy_any_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.any_200.status_code}"
+}
+
+/// configure root options
+resource "aws_api_gateway_method" "proxy_root_options_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id   = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "root_options_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_options_method.http_method}"
+  status_code = "200"
+
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "root_options_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_options_method.http_method}"
+
+  type = "MOCK"
+
+  request_templates {
+    "application/json" = "{ \"statusCode\": 200   }"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "root_options_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_options_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.root_options_200.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+/// root any method configuration
+resource "aws_api_gateway_method" "proxy_root_any_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id   = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+resource "aws_api_gateway_method_response" "root_any_200" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_any_method.http_method}"
+  status_code = "200"
+}
 
-  integration_http_method = "POST"
+resource "aws_api_gateway_integration" "root_any_integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_any_method.http_method}"
+
+  integration_http_method = "POST"                                           //Lambda function can only be invoked with POST
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.LambdaWebAPI.invoke_arn}"
 }
 
-resource "aws_api_gateway_deployment" "staging_deployment" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
-  stage_name  = "staging"
+resource "aws_api_gateway_integration_response" "root_any_integration_response" {
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
+  resource_id = "${aws_api_gateway_rest_api.MyAPIGateway.root_resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root_any_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.root_any_200.status_code}"
 }
 
+///
 resource "aws_api_gateway_deployment" "prod_deployment" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.any_integration",
+    "aws_api_gateway_integration.root_any_integration",
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.MyWebAPIGateway.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.MyAPIGateway.id}"
   stage_name  = "prod"
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowWebAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.LambdaWebAPI.arn}"
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.MyWebAPIGateway.execution_arn}/*/*"
-}
-
-output "webapi_staging_url" {
-  value = "${aws_api_gateway_deployment.staging_deployment.invoke_url}"
+  source_arn = "${aws_api_gateway_rest_api.MyAPIGateway.execution_arn}/*/*"
 }
 
 output "webapi_prod_url" {
@@ -91,5 +196,5 @@ output "webapi_prod_url" {
 }
 
 output "aws_api_gateway_rest_api_arn" {
-  value = "${aws_api_gateway_rest_api.MyWebAPIGateway.execution_arn}"
+  value = "${aws_api_gateway_rest_api.MyAPIGateway.execution_arn}"
 }
